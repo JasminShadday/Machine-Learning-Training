049b6712dd941d7a17609e5def27fd512a4cf5a8
Jasmin Shadday <112671781+JasminShadday@users.noreply.github.com>
2025-07-31 00:56:04 +0000

    aprendendo R

--- main.r
+++ main.r
@@ -1,6 +1,25 @@
 #Estudando linguagem R
+
+#operadores
 1+1
 2*2
 8/4
 
 ((20+7)/3)^2
+
+#variaveis
+objeto1 <- 3*3
+objeto2 <- 2*2
+objeto3 <- objeto1 + objeto2
+
+print(objeto3)
+
+(casa <- "todas as letras minusculas")
+(Casa <- "primeira letra maiuscula")
+
+print(casa)
+print(Casa)
+
+#bibliotecas "automaticas"
+pi
+month.name
